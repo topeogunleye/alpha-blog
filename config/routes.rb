0136263f1be499b4c576f048c5928d069392aebb
Temitope Ogunleye <topeogunleye1@gmail.com>
2022-08-15 11:45:39 +0100

    Make app production ready

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  get 'about', to: 'pages#about'
-  # Defines the root path route ("/")
-  # root "articles#index"
   root "pages#home"
+  get 'about', to: 'pages#about'
 end
